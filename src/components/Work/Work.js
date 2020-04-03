f64029230e12097485e7b4ffd6ee2ffc777d594d
pz1996r <pz1996r@gmail.com>
2020-04-03 18:21:11 +0200

    error checking

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -27,7 +27,7 @@ const Project = styled.div`
 const Project2 = styled.div`
     width:100%;
     padding:30px;
-    background-color: ${({ theme }) => theme.colors.tertiary};
+    background-color: ${({ theme }) => theme.colors.secondary};
     color: ${({ theme }) => theme.colors.primaryTransparent};
 
 
